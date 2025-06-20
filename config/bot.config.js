a48aef65ed83c5da54386bfe885277885b34f7c8
Daniel Eugenia <165719411+d4nieleugeni4@users.noreply.github.com>
2025-06-20 20:31:33 -0300

    Update bot.config.js

--- config/bot.config.js
+++ config/bot.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  dono: "5511999999999",           // Número do dono (com DDI e DDD)
-  bot: "5511988888888",            // Número do bot (pode ser o mesmo se for pessoal)
+  dono: "5524981321901",           // Número do dono (com DDI e DDD)
+  bot: "5511963546094",            // Número do bot (pode ser o mesmo se for pessoal)
   prefixo: ".",                    // Prefixo dos comandos
   versao: "1.0.0",                 // Versão do bot
 };
