438217522a21c683434cf302d710889874d5f78e
Daniel Eugenia <165719411+d4nieleugeni4@users.noreply.github.com>
2025-07-13 16:45:53 -0300

    Delete comandos directory

--- comandos/membro/ping.js
+++
@@ -1,9 +0,0 @@
-const { prefixo } = require("../../config/config");
-
-module.exports = {
-  comando: "ping",
-  exec: async (sock, m) => {
-    const from = m.key.remoteJid;
-    await sock.sendMessage(from, { text: `pong! [prefixo: ${prefixo}]` });
-  }
-};
